47cef9194bf9a52b61e053962e8247d155191e2b
Lumine8 <sankargopan1@gmail.com>
2022-01-24 21:04:46 +0530

    update: the website works properly

--- app.js
+++ app.js
@@ -2,9 +2,27 @@ var btntranslate = document.querySelector("#btn-translate");
 var txtinput = document.querySelector("#txt-input");
 var txtoutput = document.querySelector("#txtoutput")
 
+var serverurl = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslation(text){
+    return serverurl + "?" + "text="+text
+}
+
+function errorhandler(error){
+    console.log("error occured!!", error)
+    alert("Something wrong with server!! Try again after some time.")
+}
 
 function clickEventHandler(){
-txtoutput.innerText = "tranlated "+txtinput.value
+
+var inputtxt = txtinput.value;
+
+fetch(getTranslation(inputtxt))
+.then(response => response.json())
+.then(json =>{ 
+            var translatedText = json.contents.translated
+                txtoutput.innerText = translatedText})
+.catch(errorhandler)
 }
 
 btntranslate.addEventListener("click",clickEventHandler)
